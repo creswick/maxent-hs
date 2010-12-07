5d4c076289a76dd7d93b815dbdf804e7b13bab60
Daniel de Kok <me@danieldk.eu>
2010-12-07 11:43:54 +0100

    Move internal data structures to Numeric.Maxent.Train.Internal.

--- Numeric/MaxEnt/Train.hs
+++ Numeric/MaxEnt/Train.hs
@@ -1,6 +1,13 @@
-module Numeric.MaxEnt.Train (EstimateData(..), FeatureValues, TrainCorpus(..),
-                             TrainContext(..), TrainEvent(..), estimate,
-                             estimateBy, progress_silent,
+-- |
+-- Module      : Numeric.MaxEnt.Train
+-- Copyright   : (c) 2010 Daniël de Kok
+-- License     : Apache 2
+--
+-- Maintainer  : Daniël de Kok <me@danieldk.eu>
+-- Stability   : experimental
+
+module Numeric.MaxEnt.Train (EstimateData(..), FeatureValues, TrainCorpus,
+                             estimate, estimateBy, progress_silent,
                              progress_verbose, toTrainCorpus) where
 
 import Control.Monad (forM_, foldM, liftM, mapM, liftM)
@@ -19,26 +26,9 @@ import System.IO (stderr)
 import Text.Printf
 
 import Numeric.MaxEnt
-
-data TrainCorpus a = TrainCorpus (FeatureMapping a) [TrainContext]
-
-data TrainContext = TrainContext {
-      trainCtxP      :: Double,
-      trainCtxEvents :: [TrainEvent]
-} deriving (Show)
-
-data TrainEvent = TrainEvent {
-      trainEvtP             :: Double,
-      trainEvtFeatureValues :: [(Int, Double)]
-} deriving (Show)
+import Numeric.MaxEnt.Train.Internal
 
 type FeatureValues = V.Vector Double
-
-data FeatureMapping a = FeatureMapping {
-      featureInt   :: M.Map a Int,
-      intFeature   :: M.Map Int a,
-      featureCount :: Int
-}
 
 -- | Data that is required during optimization.
 data EstimateData = EstimateData [TrainContext] FeatureValues
@@ -68,43 +58,6 @@ estimateBy progress (TrainCorpus featureMapping corpus) nFeatures = do
           lbfgsData = EstimateData corpus fVals
           n2f = numbersToFeatures featureMapping
 
-emptyMapping :: (FeatureMapping a, [TrainContext])
-emptyMapping = (FeatureMapping M.empty M.empty (-1), [])
-
-ctxsToNum :: Ord a => (FeatureMapping a, [TrainContext]) -> [Context a] ->
-             (FeatureMapping a, [TrainContext])
-ctxsToNum acc = foldl ctxToNum acc
-
-ctxToNum :: Ord a => (FeatureMapping a, [TrainContext]) -> Context a ->
-            (FeatureMapping a, [TrainContext])
-ctxToNum (m, ctxs) (Context evts) = (newM, TrainContext scoreSum trainEvts:ctxs)
-    where scoreSum = sumScores evts
-          (newM, trainEvts) = foldl evtToNum (m, []) evts
-
-
-evtToNum :: Ord a => (FeatureMapping a, [TrainEvent]) -> Event a ->
-            (FeatureMapping a, [TrainEvent])
-evtToNum (m, evts) (Event score fVals) = (newM, evt:evts)
-    where (newM, newFVals) = fsToNum m fVals
-          evt = TrainEvent score newFVals
-
-fsToNum :: Ord a => FeatureMapping a -> [(a, Double)] ->
-           (FeatureMapping a, [(Int, Double)])
-fsToNum m = foldl fToNum (m, [])
-
-fToNum :: Ord a => (FeatureMapping a, [(Int, Double)]) -> (a, Double) ->
-          (FeatureMapping a, [(Int, Double)])
-fToNum (m@(FeatureMapping fInt intF count), acc) (f, val) =
-    case M.lookup f fInt of
-      Just i -> (m, (i, val):acc)
-      Nothing -> (newMapping, (newCount, val):acc)
-          where newCount = count + 1
-                newMapping = FeatureMapping (M.insert f newCount fInt)
-                                            (M.insert newCount f intF)
-                                            newCount
-
-sumScores :: [Event a] -> Double 
-sumScores = foldl (\acc (Event score _) -> acc + score) 0.0
 
 numbersToFeatures :: Ord a => FeatureMapping a -> [Double] ->
                      M.Map a Double
@@ -115,17 +68,6 @@ numbersToFeatures (FeatureMapping _ intToFeatureMapping _) weights =
               where f = case M.lookup idx intToFeatureMapping of
                           Just val -> val
                           Nothing  -> error "Feature does not occur in mapping."
-
--- Normalize the scores of a training corpus, giving a real probability
--- distribution for contexts and events.
-normalizeTrainCorpus :: [TrainContext] -> [TrainContext]
-normalizeTrainCorpus ctxs = map (normalizeCtx (scoreSum ctxs)) ctxs
-    where scoreSum = foldl (\acc (TrainContext score _) -> acc + score) 0.0
-          normalizeCtx sum (TrainContext score evts) =
-              TrainContext (score / sum) $ map (normalizeEvt sum) evts
-          normalizeEvt sum (TrainEvent score fVals) =
-              TrainEvent (score / sum) fVals
-
 
 toTrainCorpus :: Ord a => [Context a] -> TrainCorpus a
 toTrainCorpus ctxs = TrainCorpus mapping (normalizeTrainCorpus corpus)
---
+++ Numeric/MaxEnt/Train/Internal.hs
@@ -0,0 +1,88 @@
+-- |
+-- Module      : Numeric.MaxEnt.Train.Internal
+-- Copyright   : (c) 2010 Daniël de Kok
+-- License     : Apache 2
+--
+-- Maintainer  : Daniël de Kok <me@danieldk.eu>
+-- Stability   : experimental
+--
+-- Non-public data structures for training maximum entropy models.
+
+module Numeric.MaxEnt.Train.Internal (FeatureMapping(..),
+                                      TrainContext(..),
+                                      TrainCorpus(..),
+                                      TrainEvent(..),
+                                      ctxsToNum,
+                                      emptyMapping,
+                                      normalizeTrainCorpus,
+                                     ) where
+
+import qualified Data.Map as M
+
+import Numeric.MaxEnt
+
+data TrainCorpus a = TrainCorpus (FeatureMapping a) [TrainContext]
+
+data FeatureMapping a = FeatureMapping {
+      featureInt   :: M.Map a Int,
+      intFeature   :: M.Map Int a,
+      featureCount :: Int
+}
+
+data TrainContext = TrainContext {
+      trainCtxP      :: Double,
+      trainCtxEvents :: [TrainEvent]
+} deriving (Show)
+
+data TrainEvent = TrainEvent {
+      trainEvtP             :: Double,
+      trainEvtFeatureValues :: [(Int, Double)]
+} deriving (Show)
+
+emptyMapping :: (FeatureMapping a, [TrainContext])
+emptyMapping = (FeatureMapping M.empty M.empty (-1), [])
+
+ctxsToNum :: Ord a => (FeatureMapping a, [TrainContext]) -> [Context a] ->
+             (FeatureMapping a, [TrainContext])
+ctxsToNum acc = foldl ctxToNum acc
+
+ctxToNum :: Ord a => (FeatureMapping a, [TrainContext]) -> Context a ->
+            (FeatureMapping a, [TrainContext])
+ctxToNum (m, ctxs) (Context evts) = (newM, TrainContext scoreSum trainEvts:ctxs)
+    where scoreSum = sumScores evts
+          (newM, trainEvts) = foldl evtToNum (m, []) evts
+
+
+evtToNum :: Ord a => (FeatureMapping a, [TrainEvent]) -> Event a ->
+            (FeatureMapping a, [TrainEvent])
+evtToNum (m, evts) (Event score fVals) = (newM, evt:evts)
+    where (newM, newFVals) = fsToNum m fVals
+          evt = TrainEvent score newFVals
+
+fsToNum :: Ord a => FeatureMapping a -> [(a, Double)] ->
+           (FeatureMapping a, [(Int, Double)])
+fsToNum m = foldl fToNum (m, [])
+
+fToNum :: Ord a => (FeatureMapping a, [(Int, Double)]) -> (a, Double) ->
+          (FeatureMapping a, [(Int, Double)])
+fToNum (m@(FeatureMapping fInt intF count), acc) (f, val) =
+    case M.lookup f fInt of
+      Just i -> (m, (i, val):acc)
+      Nothing -> (newMapping, (newCount, val):acc)
+          where newCount = count + 1
+                newMapping = FeatureMapping (M.insert f newCount fInt)
+                                            (M.insert newCount f intF)
+                                            newCount
+
+sumScores :: [Event a] -> Double 
+sumScores = foldl (\acc (Event score _) -> acc + score) 0.0
+
+-- Normalize the scores of a training corpus, giving a real probability
+-- distribution for contexts and events.
+normalizeTrainCorpus :: [TrainContext] -> [TrainContext]
+normalizeTrainCorpus ctxs = map (normalizeCtx (scoreSum ctxs)) ctxs
+    where scoreSum = foldl (\acc (TrainContext score _) -> acc + score) 0.0
+          normalizeCtx sum (TrainContext score evts) =
+              TrainContext (score / sum) $ map (normalizeEvt sum) evts
+          normalizeEvt sum (TrainEvent score fVals) =
+              TrainEvent (score / sum) fVals
